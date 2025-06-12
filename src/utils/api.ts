728b46af879264dd2d8ee009769f926c5580ff83
Olubukun Samuel <samuelolubukun@gmail.com>
2025-06-12 11:25:13 +0100

    Update api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://melvisai.onrender.com/api',
+  baseURL: 'http://localhost:8000/api',
 });
 
 API.interceptors.request.use((config) => {
